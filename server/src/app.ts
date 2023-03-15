627cd028dad40bd896c54272a2081fad54278eb8
madahva <galarzaguillermo.ggm@gmail.com>
2023-03-15 09:00:07 -0300

    Fix (app): Change headers policy

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,25 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import morgan from "morgan";
-import dotenv from "dotenv"
-import routes from "./routes/index"
+import dotenv from "dotenv";
+import routes from "./routes/index";
 
-dotenv.config()
+dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use("/", routes)
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+app.use("/", routes);
 
 export default app;

8cb01e3debd4ce069063f8c57b71a5208573fda5
madahva <galarzaguillermo.ggm@gmail.com>
2023-05-19 22:16:03 -0300

    Deploy

--- server/src/db.ts
+++ server/src/db.ts
@@ -6,9 +6,6 @@ const deploy: any = process.env.DATABASE_URL;
 
 export const sequelize = new Sequelize(deploy, {
   dialect: "postgres",
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  username: process.env.PGUSER,
   models: [__dirname + "/models"],
 });
 
